2458430975da3be8d0d837b365deff894eb83b0d
Михаил <messiahgamedesign@gmail.com>
2020-05-23 19:49:59 +0300

    Rewrite start code

--- medium8-1-5/Program.cs
+++ medium8-1-5/Program.cs
@@ -6,7 +6,20 @@ namespace medium8_1_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wombat");
+
+            Animal wombat = new Wombat(10, 5);
+            wombat.TakeDamage(5);
+            wombat.TakeDamage(10);
+            wombat.TakeDamage(16);
+
+            Console.WriteLine("Human");
+
+            Animal human = new Human(20, 2);
+            human.TakeDamage(14);
+            human.TakeDamage(10);
+            human.TakeDamage(15);
+            human.TakeDamage(2);
         }
     }
 }
